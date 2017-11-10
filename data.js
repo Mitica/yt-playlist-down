daee62900b1ecc8e0585ce5501b2208ff9bf5e0d
Dumitru Cantea <canteadumitru@gmail.com>
2017-11-10 18:33:56 +0200

    download users playlists

--- data.js
+++ data.js
@@ -1,12 +1,21 @@
 
 const fs = require('fs');
 const path = require('path');
+const Url = require('url');
 
 exports.getSourceNames = function getSourceNames() {
     return fs.readdirSync(path.join(__dirname, 'sources'), 'utf8').map(file => path.basename(file, '.txt'));
 }
 
-exports.getPlaylists = function getPlaylists(sourceName) {
+exports.getUrls = function getItems(sourceName) {
     const p = path.join(__dirname, 'sources', sourceName + '.txt');
     return fs.readFileSync(p, 'utf8').split('\n').filter(line => line && line.length > 10);
+}
+
+exports.isPlaylistUrl = function (item) {
+    return typeof Url.parse(item, true).query.list === 'string';
+}
+
+exports.isUserUrl = function (item) {
+    return /\/user\//.test(item);
 }
--- index.js
+++ index.js
@@ -3,24 +3,26 @@ const path = require('path');
 const ytdl = require('youtube-dl');
 const data = require('./data');
 const Url = require('url');
-const {sequence} = require('./utils');
+const { sequence } = require('./utils');
 
 const START_PLAYLIST_ID = process.env.START_PLAYLIST_ID;
+const SOURCE = process.env.SOURCE;
+
 let startedPlaylistId = false;
 
 function playlistDownloader(name, url, dir) {
     console.log(`Starting download url=${url}, name=${name}, dir=${dir}`);
     if (START_PLAYLIST_ID && !startedPlaylistId) {
-        if(Url.parse(url, true).query.list === START_PLAYLIST_ID) {
-            console.log('START from PLAYLIST ID='+START_PLAYLIST_ID);
+        if (Url.parse(url, true).query.list === START_PLAYLIST_ID) {
+            console.log('START from PLAYLIST ID=' + START_PLAYLIST_ID);
             startedPlaylistId = true;
         } else {
-            console.log('Not found START PLAYLIST ID='+START_PLAYLIST_ID);
+            console.log('Not found START PLAYLIST ID=' + START_PLAYLIST_ID);
             return Promise.resolve();
         }
     }
     return new Promise((resolve, reject) => {
-        ytdl.exec(url, ['--ignore-config', '--yes-playlist', '-r', '3M', '--fragment-retries', '10', '-f', 'mp4', '--all-subs', '--audio-format', 'mp3', '-o', path.join(dir, name,'%(playlist_title)s','%(title)s.%(ext)s')], {}, function (err, output) {
+        ytdl.exec(url, ['--ignore-config', '--yes-playlist', '-r', '3M', '--fragment-retries', '10', '-f', 'mp4', '--all-subs', '--audio-format', 'mp3', '-o', path.join(dir, name, '%(playlist_title)s', '%(title)s.%(ext)s')], {}, function (err, output) {
             if (err) {
                 return reject(err);
             }
@@ -29,16 +31,40 @@ function playlistDownloader(name, url, dir) {
     });
 }
 
+function userDownloader(name, url, dir) {
+    console.log(`Starting download url=${url}, name=${name}, dir=${dir}`);
+
+    return new Promise((resolve, reject) => {
+        ytdl.exec(url, ['--ignore-config', '-r', '3M', '--fragment-retries', '10', '-f', 'mp4', '--audio-format', 'mp3', '-o', path.join(dir, name, '%(playlist_title)s', '%(title)s.%(ext)s'), url], {}, function (err, output) {
+            if (err) {
+                return reject(err);
+            }
+            resolve(output);
+        });
+    });
+}
+
+function urlDownloader(name, url, dir) {
+    if (!dir) {
+        return Promise.reject(new Error('Output Dir is required!'));
+    }
+
+    if (data.isUserUrl(url)) {
+        return userDownloader(name, url, dir);
+    }
+    return playlistDownloader(name, url, dir);
+}
+
 function downloadSource(sourceName) {
     console.log('Starting download source ' + sourceName);
-    const playlists = data.getPlaylists(sourceName);
+    const urls = data.getUrls(sourceName);
 
-    return sequence(playlists, url => playlistDownloader(sourceName, url, process.env.OUTPUT_DIR));
+    return sequence(urls, url => urlDownloader(sourceName, url, process.env.OUTPUT_DIR));
 }
 
 
 function start() {
-    const sources = data.getSourceNames();
+    const sources = typeof SOURCE === 'string' ? [SOURCE] : data.getSourceNames();
 
     return sequence(sources, source => downloadSource(source));
 }
